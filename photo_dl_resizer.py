adb992191582742185d62322809d51f39b4d6fe2
Eric Meas <emmeas@gmail.com>
2022-04-17 15:01:40 -0700

    Create photo_dl_resizer.py
    Created file

---
+++ photo_dl_resizer.py
@@ -0,0 +1,9 @@
+"""
+Created: April 17, 2022, 03:00:00 PM (Sunday)
+Author: Eric Meas
+Contact:
+Copyright: Copyright (C) 2022 Eric Meas All rights reserved
+License: GNU GPLv3
+Version: 1.0
+https://www.gnu.org/licenses/gpl-3.0
+"""
