7b740694fb9c33415c7912d8ed73347116bf1f38
Eric Meas <emmeas@gmail.com>
2022-04-18 20:51:38 -0700

    Update photo_dl_resizer.py
    created for loop and import os and moved PIL code into for loop

--- photo_dl_resizer.py
+++ photo_dl_resizer.py
@@ -8,7 +8,15 @@ Version: 1.0
 https://www.gnu.org/licenses/gpl-3.0
 """
 
+import os
+
 from PIL import Image
-im = Image.open("*.jpg")
-resize = im.resize((1100,800),Image.Resampling.LANCZOS)
-resize.save("*.jpg")
+
+Input = "C:/Users/Eric/Desktop/Images"
+
+for file in os.listdir(Input):
+  if file.endswith(".jpg"):
+    im = Image.open(Input + "/{}".format(file))
+    resize = im.resize((1100,800),Image.Resampling.LANCZOS)
+    file_ext = os.path.splitext(file)
+    resize.save("Copy/{}.jpg".format(file_ext))
